1b97bb70e6893382b438a276c4be78c2613710d0
Md Irshad <32608381+irshadmd@users.noreply.github.com>
2020-12-29 19:44:31 +0530

    day2

--- farmersFriend/views.py
+++ farmersFriend/views.py
@@ -1,4 +1,19 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse('Home')
+    return render(request,'index.html')
+
+def index(request):
+    return render(request,'index.html')
+
+def registerpage(request):
+    return render(request,'register.html')
+
+def askquestion(request):
+    return render(request,'askquestion.html')
+
+def weather(request):
+    return render(request,'weather.html')
+
+def news(request):
+    return render(request,'news.html')
